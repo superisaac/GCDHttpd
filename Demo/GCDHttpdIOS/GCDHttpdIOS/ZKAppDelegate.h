490fde38b073bd2df6fc019011f63fe766424529
Zeng ke <zengke@ujuhui.com>
2013-02-22 12:28:04 +0800

    Added missing files on iOS Demo

---
+++ Demo/GCDHttpdIOS/GCDHttpdIOS/ZKAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ZKAppDelegate.h
+//  GCDHttpdIOS
+//
+//  Created by 曾科 on 13-2-22.
+//  Copyright (c) 2013年 zengke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UIViewController *viewController;
+
+@end
---
+++ Demo/GCDHttpdIOS/GCDHttpdIOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GCDHttpdIOS
+//
+//  Created by 曾科 on 13-2-22.
+//  Copyright (c) 2013年 zengke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZKAppDelegate class]));
+    }
+}
